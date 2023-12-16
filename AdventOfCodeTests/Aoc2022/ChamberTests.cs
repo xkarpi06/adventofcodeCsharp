8ab61f9a0731800d11d752bbb4399f879665b88e
Jakub Karpíšek <jakub.karpisek@notum.cz>
2023-12-16 16:04:51 +0100

    simplify expected state in ChamberTests

--- AdventOfCodeTests/Aoc2022/ChamberTests.cs
+++ AdventOfCodeTests/Aoc2022/ChamberTests.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using AdventOfCode.Helpers;
 
-namespace AdventOfCode.Aoc2022.Day17.Tests
+namespace AdventOfCodeTests.Aoc2022
 {
 
     /*
@@ -24,6 +24,7 @@ namespace AdventOfCode.Aoc2022.Day17.Tests
         [TestInitialize]
         public void TestInitialize()
         {
+            // _chamber = new ByteChamber3();
             _chamber = new ByteChamber2();
             // _chamber = new ByteChamber();
             // _chamber = new SlowChamber();
@@ -42,17 +43,15 @@ namespace AdventOfCode.Aoc2022.Day17.Tests
         {
             _chamber.SpawnRock(Rock.PLUS);
             _chamber.PutRockToSleep();
-            List<List<int>> ExpectedState = new()
-            {
-                // 0 - empty space, 1 - dead rock
-                new() {0,0,0,1,0,0,0},
-                new() {0,0,1,1,1,0,0},
-                new() {0,0,0,1,0,0,0},
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,0,0,0,0},
-            };
-            Assert.IsTrue(ChamberEquality(ToBoolMatrix(ExpectedState), _chamber.State));
+            const string expectedState = """
+                                         ...X...
+                                         ..XXX..
+                                         ...X...
+                                         .......
+                                         .......
+                                         .......
+                                         """;
+            Assert.IsTrue(ChamberEquality(ToBoolMatrix(expectedState), _chamber.State));
         }
 
         /*
@@ -79,19 +78,17 @@ namespace AdventOfCode.Aoc2022.Day17.Tests
             bool x = _chamber.MoveRockWithRules(Dir.DOWN);    // this move is already into ground, should not be executed
             Assert.IsFalse(x);
             _chamber.PutRockToSleep();
-            List<List<int>> ExpectedState = new()
-            {
-                // 0 - empty space, 1 - dead rock
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,1,0,0,0},
-                new() {0,0,1,1,1,0,0},
-                new() {0,0,0,1,0,0,0},
-            };
+            const string expectedState = """
+                                         .......
+                                         .......
+                                         .......
+                                         ...X...
+                                         ..XXX..
+                                         ...X...
+                                         """;
             PrintPrettyBool(_chamber.State);
-            PrintPrettyInt(ExpectedState);
-            Assert.IsTrue(ChamberEquality(ToBoolMatrix(ExpectedState), _chamber.State));
+            PrintPrettyString(expectedState);
+            Assert.IsTrue(ChamberEquality(ToBoolMatrix(expectedState), _chamber.State));
             Assert.AreEqual(3L, _chamber.RockTowerHeight);
         }
 
@@ -113,29 +110,48 @@ namespace AdventOfCode.Aoc2022.Day17.Tests
             bool x = _chamber.MoveRockWithRules(Dir.DOWN); // this move will collide with existing rock tower
             Assert.IsFalse(x);
             _chamber.PutRockToSleep();
-            List<List<int>> ExpectedState = new()
-            {
-                // 0 - empty space, 1 - dead rock
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,1,1,0,0,0},
-                new() {0,0,1,1,0,0,0},
-                new() {0,0,0,1,0,0,0},
-                new() {0,0,1,1,1,0,0},
-                new() {0,0,0,1,0,0,0},
-            };
-            // TODO: represent like this:
-            var todo = """
-                       .......
-                       ..BB...
-                       ..BB...
-                       ...A...
-                       ..AAA..
-                       ...A...
-                       """;
-            Assert.IsTrue(ChamberEquality(ToBoolMatrix(ExpectedState), _chamber.State));
+            const string expectedState = """
+                                         .......
+                                         .......
+                                         .......
+                                         ..BB...
+                                         ..BB...
+                                         ...A...
+                                         ..AAA..
+                                         ...A...
+                                         """;
+            Assert.IsTrue(ChamberEquality(ToBoolMatrix(expectedState), _chamber.State));
             Assert.AreEqual(5L, _chamber.RockTowerHeight);
+        }
+
+        /*
+         * Spawn rock A that will be higher than rock B. Put rock A to sleep next to a wall. Rock B will spawn
+         * away from that wall and should not have a problem to move towards it. (This was a bu.g in ByteChamber3)
+         */
+        [TestMethod()]
+        public void MoveRockWithRulesTest_PreviousRockDoesNotEffectCurrent()
+        {
+            _chamber.SpawnRock(Rock.SQUARE);
+            _chamber.MoveRockWithRules(Dir.LEFT);
+            _chamber.MoveRockWithRules(Dir.LEFT); 
+            _chamber.MoveRockWithRules(Dir.DOWN);
+            _chamber.MoveRockWithRules(Dir.DOWN);
+            _chamber.MoveRockWithRules(Dir.DOWN);
+            _chamber.PutRockToSleep();
+            _chamber.SpawnRock(Rock.MINUS);
+            _chamber.MoveRockWithRules(Dir.LEFT);
+            _chamber.PutRockToSleep();
+            const string expectedState = """
+                                         .BBBB..
+                                         .......
+                                         .......
+                                         .......
+                                         AA.....
+                                         AA.....
+                                         """;
+            PrintPrettyBool(_chamber.State);
+            PrintPrettyString(expectedState);
+            Assert.IsTrue(ChamberEquality(ToBoolMatrix(expectedState), _chamber.State));
         }
 
         /*
@@ -151,17 +167,15 @@ namespace AdventOfCode.Aoc2022.Day17.Tests
             Assert.IsTrue(x);   // side collision does not prevent down movement
             _chamber.PutRockToSleep();
 
-            List<List<int>> ExpectedState = new()
-            {
-                // 0 - empty space, 1 - dead rock
-                new() {1,1,0,0,0,0,0},
-                new() {1,1,0,0,0,0,0},
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,0,0,0,0},
-            };
+            const string expectedState = """
+                                         XX.....
+                                         XX.....
+                                         .......
+                                         .......
+                                         .......
+                                         """;
             
-            Assert.IsTrue(ChamberEquality(ToBoolMatrix(ExpectedState), _chamber.State));
+            Assert.IsTrue(ChamberEquality(ToBoolMatrix(expectedState), _chamber.State));
         }
 
         /*
@@ -176,16 +190,14 @@ namespace AdventOfCode.Aoc2022.Day17.Tests
             Assert.IsTrue(x);   // side collision does not prevent down movement
             _chamber.PutRockToSleep();
 
-            List<List<int>> ExpectedState = new()
-            {
-                // 0 - empty space, 1 - dead rock
-                new() {0,0,0,1,1,1,1},
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,0,0,0,0},
-            };
-
-            Assert.IsTrue(ChamberEquality(ToBoolMatrix(ExpectedState), _chamber.State));
+            const string expectedState = """
+                                         ...XXXX
+                                         .......
+                                         .......
+                                         .......
+                                         """;
+            
+            Assert.IsTrue(ChamberEquality(ToBoolMatrix(expectedState), _chamber.State));
         }
 
         /*
@@ -210,20 +222,19 @@ namespace AdventOfCode.Aoc2022.Day17.Tests
             Assert.IsTrue(x);   // side collision does not prevent down movement
             _chamber.PutRockToSleep();
 
-            List<List<int>> ExpectedState = new()
-            {
-                // 0 - empty space, 1 - dead rock
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,1,0,0,0},
-                new() {0,0,1,1,1,0,0},
-                new() {0,0,0,1,1,0,0},
-                new() {0,0,0,1,1,1,0},
-                new() {0,0,0,0,1,0,0},
-            };
-            Assert.IsTrue(ChamberEquality(ToBoolMatrix(ExpectedState), _chamber.State));
+            const string expectedState = """
+                                         .......
+                                         .......
+                                         .......
+                                         .......
+                                         ...B...
+                                         ..BBB..
+                                         ...BA..
+                                         ...AAA.
+                                         ....A..
+                                         """;
+            
+            Assert.IsTrue(ChamberEquality(ToBoolMatrix(expectedState), _chamber.State));
             Assert.AreEqual(5L, _chamber.RockTowerHeight);
         }
 
@@ -250,19 +261,18 @@ namespace AdventOfCode.Aoc2022.Day17.Tests
             Assert.IsTrue(x);   // side collision does not prevent down movement
             _chamber.PutRockToSleep();
 
-            List<List<int>> ExpectedState = new()
-            {
-                // 0 - empty space, 1 - dead rock
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,1,1,0,0},
-                new() {0,0,1,1,1,0,0},
-                new() {0,1,1,1,0,0,0},
-                new() {0,0,1,0,0,0,0},
-            };
-            Assert.IsTrue(ChamberEquality(ToBoolMatrix(ExpectedState), _chamber.State));
+            const string expectedState = """
+                                         .......
+                                         .......
+                                         .......
+                                         .......
+                                         ...BB..
+                                         ..ABB..
+                                         .AAA...
+                                         ..A....
+                                         """;
+
+            Assert.IsTrue(ChamberEquality(ToBoolMatrix(expectedState), _chamber.State));
             Assert.AreEqual(4L, _chamber.RockTowerHeight);
         }
 
@@ -272,19 +282,18 @@ namespace AdventOfCode.Aoc2022.Day17.Tests
             _chamber.SpawnRock(Rock.MINUS);
             _chamber.PutRockToSleep();
 
-            List<List<int>> ExpectedState = new()
-            {
-                // 0 - empty space, 1 - dead rock
-                new() {0,0,1,1,1,1,0},
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,0,0,0,0},
-            };
-            Assert.IsTrue(ChamberEquality(ToBoolMatrix(ExpectedState), _chamber.State));
+            const string expectedState = """
+                                         ..XXXX.
+                                         .......
+                                         .......
+                                         .......
+                                         """;
+
+            Assert.IsTrue(ChamberEquality(ToBoolMatrix(expectedState), _chamber.State));
         }
 
         /*
-         * Create unreachable part of a cave, then try to remove it. Validate chamber state.
+         * Create unreachable part of a chamber, then try to remove it. Validate chamber state.
          */
         [TestMethod()]
         public void RemoveUnreachablePartOfChamberTest()
@@ -308,40 +317,38 @@ namespace AdventOfCode.Aoc2022.Day17.Tests
             _chamber.MoveRockWithRules(Dir.DOWN);
             _chamber.MoveRockWithRules(Dir.DOWN);
             _chamber.PutRockToSleep();
-            List<List<int>> ExpectedState = new()
-            {
-                // 0 - empty space, 1 - dead rock
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,0,0,0,0},
-                new() {1,1,1,1,0,0,0},
-                new() {0,0,0,1,1,1,1},
-                new() {0,0,0,1,0,0,0},
-                new() {0,0,0,1,0,0,0},
-                new() {0,0,0,1,0,0,0},
-                new() {0,0,0,1,0,0,0},
-            };
-            Assert.IsTrue(ChamberEquality(ToBoolMatrix(ExpectedState), _chamber.State));
+            var expectedState = """
+                                .......
+                                .......
+                                .......
+                                CCCC...
+                                ...BBBB
+                                ...A...
+                                ...A...
+                                ...A...
+                                ...A...
+                                """;
+            Assert.IsTrue(ChamberEquality(ToBoolMatrix(expectedState), _chamber.State));
             Assert.AreEqual(6L, _chamber.RockTowerHeight);
             _chamber.RemoveUnreachablePartOfChamber();
-            ExpectedState = new()
-            {
-                // 0 - empty space, 1 - dead rock
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,0,0,0,0},
-                new() {0,0,0,0,0,0,0},
-                new() {1,1,1,1,0,0,0},
-            };
-            Assert.IsTrue(ChamberEquality(ToBoolMatrix(ExpectedState), _chamber.State));
+            expectedState = """
+                            .......
+                            .......
+                            .......
+                            CCCC...
+                            """;
+            PrintPrettyBool(_chamber.State);
+            PrintPrettyString(expectedState);
+            Assert.IsTrue(ChamberEquality(ToBoolMatrix(expectedState), _chamber.State));
             Assert.AreEqual(6L, _chamber.RockTowerHeight);
         }
 
-        private static void PrintPrettyInt(List<List<int>> list, string delimiter = "")
+        private void PrintPrettyString(string input)
         {
-            PrintPretty(list.Select(x => x.Select(y => (y > 0) ? 'X' : '.').ToList()).ToList());
+            PrintPrettyBool(ToBoolMatrix(input));
         }
 
-        private static void PrintPrettyBool(List<List<bool>> list, string delimiter = "")
+        private void PrintPrettyBool(List<List<bool>> list, string delimiter = "")
         {
             PrintPretty(list.Select(x => x.Select(y => y ? 'X' : '.').ToList()).ToList());
         }
@@ -349,7 +356,7 @@ namespace AdventOfCode.Aoc2022.Day17.Tests
         /*
          * Util function for printing 2D matrices.
          */
-        private static void PrintPretty<T>(List<List<T>> list, string delimiter = "")
+        private void PrintPretty<T>(List<List<T>> list, string delimiter = "")
         {
             foreach (List<T> row in list)
             {
@@ -359,6 +366,17 @@ namespace AdventOfCode.Aoc2022.Day17.Tests
 
         private List<List<bool>> ToBoolMatrix(List<List<int>> input) =>
             input.Select(row => row.Select(col => col == 1).ToList()).ToList();
+
+        /*
+         * Anything other than '.' will be true. '.' is false.
+         */
+        private List<List<bool>> ToBoolMatrix(string input)
+        {
+            const char EMPTY = '.';
+            return input.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).Select(
+                line => line.Select(c => c != EMPTY).ToList()
+            ).ToList();
+        }
 
         private bool ChamberEquality(List<List<bool>> expected, List<List<bool>> actual) =>
             expected.SequenceEqual(actual, new ListEqualityComparer<bool>());
