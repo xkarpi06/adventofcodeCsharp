6fb8f825ce14472a428c1acd74b7045323898a34
Jakub Karpíšek <jakub.karpisek@notum.cz>
2023-12-16 16:00:39 +0100

    add BenchmarkDotNet & README.md

---
+++ AoCBenchmark/Benchmark.cs
@@ -0,0 +1,44 @@
+﻿using AdventOfCode.Aoc2022.Day17;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace AoCBenchmark
+{
+    public class CompareChambers
+    {
+        private IChamber _chamber;
+
+        [Params(1_000_000)]
+        public int N;
+
+        [Benchmark(Baseline = true)]
+        public void SlowChamber()
+        {
+            _chamber = new SlowChamber();
+            Day17.Run(_chamber, N);
+        }
+        
+        [Benchmark]
+        public void ByteChamber()
+        {
+            _chamber = new ByteChamber();
+            Day17.Run(_chamber, N);
+        }
+
+        [Benchmark]
+        public void ByteChamber2()
+        {
+            _chamber = new ByteChamber2();
+            Day17.Run(_chamber, N);
+        }
+    }
+
+    public class Benchmark
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<CompareChambers>();
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,11 @@
+# adventofcode
+Advent of Code coding challenge (https://adventofcode.com)
+
+# benchmark
+to compare several implementations, benchmark can be used
+
+to run benchmark, first install BenchmarkDotNet package
+- Navigate to AoCBenchmark directory in terminal
+- run: dotnet add package BenchmarkDotNet
+run
+- dotnet run -c Release
